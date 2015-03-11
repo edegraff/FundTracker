6d510da748b1f10e6f7e21c1555180734ea50c59
LindsayL <livojevi@ualberta.ca>
2015-03-10 23:24:52 -0600

    Reporting - incorporate prediction values
    Fleshed out a bit more of FundProjector.  Created some functions and
    properties.  Still needs to be actually implemented.
    
    Reports now pull data from projections for date ranges beyond the
    current date.
    
    Reports now indicate the end of data. (And do not try to report for
    earlier unavailble data).
    
    Modified FundEntity to extrapolate value for the given day based on data
    being in the format where we only store values of funds for the days
    that CIBC releases new values.

--- FundTracker/Common/Models/Report.cs
+++ FundTracker/Common/Models/Report.cs
@@ -14,6 +14,9 @@ namespace Common.Models
         // Initialize the Report object
         public Report(DateTime start, DateTime end, List<FundEntity> funds)
         {
+            /* Initializations */
+            start = start.Date;
+            end = end.Date;
             Dates = new List<DateTime>();
             FundNames = new List<String>(); 
             FundsData = new List<String>[funds.Count];
@@ -25,14 +28,76 @@ namespace Common.Models
                 FundsData[i] = new List<String>();
             }
 
-            System.TimeSpan days = end.Subtract(start);
-            DateTime date = start;
-            Boolean includeDate;
+            int daySpan;
+            DateTime nowDate = DateTime.Now.Date;
+            DateTime date;
+            Boolean endOfData;
             float val;
 
-            for (int i = 0; i <= days.Days; i++)
+            /* Section 1, prediction values */
+            daySpan = end.Subtract(nowDate).Days;
+            date = end;
+
+            // Will we have a prediction portion?
+            if (daySpan > 0)
             {
-                includeDate = false;
+                // Initialize the projections
+                List<FundProjector> projections = new List<FundProjector>();
+                foreach (FundEntity fund in funds)
+                {
+                    projections.Add(new FundProjector(fund));
+                }
+
+                // Check if requested dates go beyond our prediction limit
+                if (daySpan > FundProjector.projectionLimit)
+                {
+                    // Set date to the first day beyond the prediction range
+                    date = date.AddDays(FundProjector.projectionLimit - daySpan + 1);
+                    Dates.Add(date);
+                    for (int f = 0; f < funds.Count; f++)
+                    {
+                        // Label the end of projection
+                        FundsData[f].Add("Projection Unavailable");
+                    }
+                    date = date.AddDays(-1);
+                }
+
+                // Set the daySpan correctly
+                if (nowDate > start.Date)
+                {
+                    daySpan = date.Subtract(nowDate).Days;
+                }
+                else 
+                {
+                    daySpan = date.Subtract(start.Date).Days + 1;
+                }
+                
+                // Fill in any prediction values
+                for (int i = 0; i < daySpan; i++)
+                {
+                    Dates.Add(date);
+                    for (int f = 0; f < funds.Count; f++)
+                    {
+                        // add the prediction value for each fund
+                        FundsData[f].Add(projections[f].getPredictionByDate(date).ToString());
+                    }
+                    date = date.AddDays(-1);
+                }
+
+                // End if the start date was in the prediction range
+                if (start > nowDate)
+                {
+                    return;
+                }
+            }
+
+            /* Section 2, past values */
+            daySpan = date.Subtract(start).Days;
+
+            for (int i = 0; i <= daySpan; i++)
+            {
+                Dates.Add(date);
+                endOfData = true;
                 for (int f = 0; f < funds.Count; f++)
                 {
                     try
@@ -40,27 +105,20 @@ namespace Common.Models
                         // Try to add the value from the date
                         val = funds[f].GetValueByDate(date);
                         FundsData[f].Add(val.ToString());
-                        includeDate = true;
+                        endOfData = false;
                     }
                     catch (InvalidOperationException e)
                     {
-                        // If no value found add "-" instead
-                        FundsData[f].Add("-");
+                        // If no value found add End of Data message instead
+                        FundsData[f].Add("Earlier Data Unavailable");
                     }
                 }
 
-                if (includeDate) // There are valid entries at date so include it
+                if (endOfData) // There are no more valid entries
                 {
-                    Dates.Add(date);
+                    break;
                 }
-                else // Remove the empty entries
-                {
-                    for (int f = 0; f < funds.Count; f++)
-                    {
-                        FundsData[f].RemoveAt(FundsData[f].Count - 1);
-                    }
-                }
-                date = date.AddDays(1);
+                date = date.AddDays(-1);
             }
         }
 
