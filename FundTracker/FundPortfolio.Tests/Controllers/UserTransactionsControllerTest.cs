78d60e7f2cfda61e1999273a5a773d0eadd01384
StevenGerdes <sgerdes@ualberta.ca>
2015-04-07 22:41:10 -0600

    Moving unfinished tests between computers

---
+++ FundTracker/FundPortfolio.Tests/Controllers/UserTransactionsControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using FundPortfolio.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace FundPortfolio.Tests.Controllers
+{
+	[TestClass]
+	public class UserTransactionsControllerTest
+	{
+		[TestMethod]
+		public void Index()
+		{
+			// Arrange
+			var controller = new UserTransactionsController();
+
+			// Act
+			ViewResult result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.IsNotNull(result);
+		}
+	}
+}
