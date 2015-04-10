ad29679c910c8fc711a735249d7ab117dd883cd8
Evan DeGraff <evandegraff@gmail.com>
2015-04-10 02:04:46 -0600

    Notifications Tests

---
+++ FundTracker/FundPortfolio.Tests/Controllers/NotificationsControllerTest.cs
@@ -0,0 +1,76 @@
+﻿using Common.Models;
+using FundPortfolio.Controllers;
+using FundPortfolio.ViewModels;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FundPortfolio.Tests.Controllers
+{
+    [TestClass]
+    public class NotificationsControllerTest
+    {
+        [TestMethod]
+        public void NotificationsController()
+        {
+            var userId = "39ee1136-9599-4f7c-8a4b-d5053bdb9690";//This refers to the user TestNotifications@fundtracker.com with password FundTracker1!
+            DeleteNotifications(userId);
+            // Preconditions
+            Assert.IsTrue(GetNotifications(userId).Count == 0);
+            
+            using (var context = ShimsContext.Create())
+            {
+
+                Microsoft.AspNet.Identity.Fakes
+                    .ShimIdentityExtensions.GetUserIdIIdentity =
+                (i) => userId;
+
+                var fakeHttpContext = new Mock<HttpContextBase>();
+                var fakeIdentity = new GenericIdentity("User");
+                var principal = new GenericPrincipal(fakeIdentity, null);
+
+                fakeHttpContext.Setup(t => t.User).Returns(principal);
+
+                var controllerContext = new Mock<ControllerContext>();
+                controllerContext.Setup(t => t.HttpContext)
+                    .Returns(fakeHttpContext.Object);
+
+                var controller = new NotificationsController();
+                controller.ControllerContext = controllerContext.Object;
+
+                var n = new ChangeNotification();
+                controller.Create(n, "511");
+                Assert.IsTrue(GetNotifications(userId).Count > 0);
+
+                controller.DeleteConfirmed(GetNotifications(userId).First().NotificationId);
+                Assert.IsTrue(GetNotifications(userId).Count == 0);
+            }
+        }
+
+        private List<Notification> GetNotifications(string userId)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Notifications.Where(n => n.UserId.Equals(userId)).ToList();
+            }
+        }
+
+        private void DeleteNotifications(string userId)
+        {
+            using (var db = new DatabaseContext())
+            {
+               db.Notifications.RemoveRange(db.Notifications.Where(n => n.UserId.Equals(userId)));
+               db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ FundTracker/FundPortfolio.Tests/NotificationsTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Common.Models;
+
+namespace FundPortfolio.Tests
+{
+    [TestClass]
+    public class NotificationsTest
+    {
+        [TestMethod]
+        public void Notifications()
+        {
+            foreach(var n in TriggerList())
+            {
+                Assert.IsTrue(n.ShouldNotify());
+            }
+
+            foreach (var n in NoTriggerList())
+            {
+                Assert.IsFalse(n.ShouldNotify());
+            }
+        }
+
+        // Returns a list of notifications that should trigger
+        public List<Notification> TriggerList()
+        {
+            List<Notification> notifications = new List<Notification>();
+
+            using (var db = new DatabaseContext())
+            {
+                var fund = db.Funds.Find("511");
+                ChangeNotification cn = new ChangeNotification();
+                cn.FundEntity = fund;
+                cn.IsPercent = false;
+                cn.ThresholdValue = 0.00000001f * (fund.CurrentValue - fund.GetValueByDate(DateTime.Now.Date - new TimeSpan(5,0,0,0,0)));
+                cn.Days = -7;
+                notifications.Add(cn);
+
+                cn = new ChangeNotification();
+                cn.FundEntity = fund;
+                cn.IsPercent = true;
+                cn.ThresholdValue = 0.00000001f * (fund.CurrentValue - fund.GetValueByDate(DateTime.Now.Date - new TimeSpan(5, 0, 0, 0, 0)));
+                cn.Days = -7;
+                notifications.Add(cn);
+
+                ValueNotification vn = new ValueNotification();
+                vn.FundEntity = fund;
+                vn.IsAbove = true;
+                vn.ThresholdValue = fund.CurrentValue - 1;
+                notifications.Add(vn);
+
+                vn = new ValueNotification();
+                vn.FundEntity = fund;
+                vn.IsAbove = false;
+                vn.ThresholdValue = fund.CurrentValue + 1;
+                notifications.Add(vn);
+
+                return notifications;
+            }
+        }
+
+        // Returns a list of notifications that should not trigger
+        public List<Notification> NoTriggerList()
+        {
+            List<Notification> notifications = new List<Notification>();
+
+            using (var db = new DatabaseContext())
+            {
+                var fund = db.Funds.Find("511");
+                ChangeNotification cn = new ChangeNotification();
+                cn.FundEntity = fund;
+                cn.IsPercent = false;
+                cn.ThresholdValue = 100;
+                cn.Days = -7;
+                notifications.Add(cn);
+
+                cn = new ChangeNotification();
+                cn.FundEntity = fund;
+                cn.IsPercent = true;
+                cn.ThresholdValue = 100;
+                cn.Days = -7;
+                notifications.Add(cn);
+
+                ValueNotification vn = new ValueNotification();
+                vn.FundEntity = fund;
+                vn.IsAbove = true;
+                vn.ThresholdValue = fund.CurrentValue + 1;
+                vn.Days = -7;
+                notifications.Add(vn);
+
+                vn = new ValueNotification();
+                vn.FundEntity = fund;
+                vn.IsAbove = false;
+                vn.ThresholdValue = fund.CurrentValue - 1;
+                vn.Days = -7;
+                notifications.Add(vn);
+
+                return notifications;
+            }
+        }
+
+    }
+}
